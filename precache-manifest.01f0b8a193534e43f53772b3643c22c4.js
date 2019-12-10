ee5c78f69f15554b401c9a7a574d80f2ca2babee
jasleenkaur37 <kaurjas68150@gmail.com>
2019-12-10 14:04:25 +0530

    Updates

---
+++ precache-manifest.01f0b8a193534e43f53772b3643c22c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b992b6f587026ede8c2983abbc40c6",
+    "url": "/phone-directory/index.html"
+  },
+  {
+    "revision": "14e9c6da59d7ba49b937",
+    "url": "/phone-directory/static/css/main.f1705b32.chunk.css"
+  },
+  {
+    "revision": "b9e9d068cb8c1ec4a7fd",
+    "url": "/phone-directory/static/js/2.d692f8df.chunk.js"
+  },
+  {
+    "revision": "14e9c6da59d7ba49b937",
+    "url": "/phone-directory/static/js/main.4ad5d9e6.chunk.js"
+  },
+  {
+    "revision": "17b8e27becdf08a6e8af",
+    "url": "/phone-directory/static/js/runtime-main.c77853c4.js"
+  }
+]);
